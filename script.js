e895017f7983eef8b432484609d3119bebba8e73
Nitin2822 <nitinkoyyana22@gmail.com>
2026-02-04 15:52:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+const btn = document.getElementById('btn');
+const output = document.getElementById('output');
+const textInput = document.getElementById('text');
+const delayInput = document.getElementById('delay');
+btn.addEventListener('click', async () => {
+    const text = textInput.value;
+    const delay = parseInt(delayInput.value);
+
+    if (!text || isNaN(delay)) return;
+
+  
+    output.innerText = "";
+
+    await sleep(delay);
+
+   
+    output.innerText = text;
+});
